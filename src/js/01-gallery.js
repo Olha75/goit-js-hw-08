11aec499526d293a5bc2e77169d06254a818e4b2
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-15 19:53:40 +0200

    8.1-z-marc

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,67 +1,64 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-
 const gallery = document.querySelector('.gallery');
-let activeLightbox = null;
+gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+gallery.addEventListener("click", onClick);
 
-
-function renderGallery() {
-    const galleryHTML = galleryItems
-      .map(
-        (item, index) => `
-          <li class="gallery__item">
-            <a class="gallery__link" href="${item.original}">
-              <img
-                class="gallery__image"
-                src="${item.preview}"
-                alt="${item.description}"
-                data-index="${index}"
-              />
-            </a>
-          </li>
-        `
-      )
-      .join('');
-    gallery.innerHTML = galleryHTML;
+function onClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return
   }
   
-  renderGallery();
-  
-  gallery.addEventListener('click', (el) => {
-    el.preventDefault();
-    const target = el.target;
-    if (target.tagName === 'IMG') {
-      const index = Number(target.dataset.index);
-      const imageSrc = galleryItems[index].original;
-  
-      const instance = SimpleLightbox.create(`
-        <div>
-          <img src="${imageSrc}" alt="${galleryItems[index].description}" />
-        </div>`,
-        {
-          onShow: (instance) => {
-            window.addEventListener('keydown', onKeyPress);
-            activeLightbox = instance;
-          },
-          onClose: (instance) => {
-            window.removeEventListener('keydown', onKeyPress);
-            activeLightbox = null;
-          },
-        }
-      );
-  
-      instance.show();
+  const currentPhoto = event.target;
+  const originalPhoto = currentPhoto.dataset.source;
+  const descriptionPhoto = currentPhoto.alt;
+
+  const instance = SimpleLightbox.create(
+    `<div>
+      <img src="${originalPhoto}" alt="${descriptionPhoto}" />
+    </div>`,
+    {
+      onShow: (instance) => {window.addEventListener("keydown", onKeyPress);},
+   
+      onClose: (instance) => {window.removeEventListener("keydown", onKeyPress);},
     }
-  });
+  );
+  instance.show();
   
   function onKeyPress(event) {
-    if (event.key === 'Escape' && activeLightbox) {
-      activeLightbox.close();
+    if (event.key === "Escape") {
+      instance.close();
     }
   }
+
+}
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => 
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+new SimpleLightbox('.gallery__item a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
   
-  console.log(galleryItems);
+console.log(galleryItems);
 
