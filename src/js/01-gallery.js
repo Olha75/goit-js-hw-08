6769fb4f4f8c085ea4b2ac39b40a4eda04f8614e
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-15 18:06:04 +0200

    8 add bibl

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,58 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// import { galleryItems } from './gallery-items';
 
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+let activeLightbox = null;
+
+function renderGallery() {
+  const galleryHTML = galleryItems
+    .map(
+      (item, index) => `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${item.original}">
+            <img
+              class="gallery__image"
+              src="${item.preview}"
+              data-source="${item.original}"
+              alt="${item.description}"
+              data-index="${index}"
+            />
+          </a>
+        </li>
+      `
+    )
+    .join('');
+  gallery.innerHTML = galleryHTML;
+}
+
+renderGallery();
+
+gallery.addEventListener('click', (el) => {
+  el.preventDefault();
+  const target = el.target;
+  if (target.tagName === 'IMG') {
+    const index = Number(target.dataset.index);
+    const imageSrc = galleryItems[index].original;
+
+    const instance = basicLightbox.create(`
+      <img src="${imageSrc}" width="800" height="600">
+    `);
+
+    instance.show();
+
+    activeLightbox = instance;
+
+    document.addEventListener('keydown', onKeyPress);
+  }
+});
+
+function onKeyPress(el) {
+  if (el.key === 'Escape' && activeLightbox) {
+    activeLightbox.close();
+    document.removeEventListener('keydown', onKeyPress);
+  }
+}
 console.log(galleryItems);
+
