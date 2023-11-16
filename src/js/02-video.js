bafed154716a337bda3c034753466f6ebad143f4
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-16 17:35:04 +0200

    8.2no-save

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,8 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
 // const player = new Player('handstick', {
 //     id: 19231868,
 //     width: 640
@@ -10,12 +12,24 @@ import throttle from 'lodash.throttle';
 //     console.log('played the video!');
 // });
 
-/* <iframe
-  id="vimeo-player"
-  src="https://player.vimeo.com/video/236203659"
-  width="640"
-  height="360"
-  frameborder="0"
-  allowfullscreen
-  allow="autoplay; encrypted-media"
-></iframe> */
+
+// <iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" width="640" height="360" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe>
+
+// <script src="https://player.vimeo.com/api/player.js"></script>
+// <script>
+//     const iframe = document.querySelector('iframe');
+const iframe = document.querySelector('#vimeo-player');
+
+    const player = new Vimeo.Player(iframe);
+    const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+   player.on('timeupdate', throttle(handleTimeupdate, 1000));
+
+function handleTimeupdate() {
+    player
+      .getCurrentTime()
+      .then(currentTime => localStorage.setItem(LOCALSTORAGE_KEY, currentTime));
+  }
+  
+  player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+  
