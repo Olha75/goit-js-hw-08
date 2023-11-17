1850ba1065b3e3578d2c7cba896fadf68088a0f8
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-17 20:45:27 +0200

    8.3-f

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,20 +4,36 @@ const form = document.querySelector(".feedback-form");
 const input = form.querySelector("input[name='email']");
 const textarea = form.querySelector("textarea[name='message']");
 
-const memoryInfo = JSON.parse(LocalStorage.getItem("feedback-form-state"))
-if(memoryInfo !==null){
-    const {email, message}=memoryInfo;
-    input.value = email;
-    textarea.value=message;
+const getFormDataFromStorage = () => {
+    const storageData = localStorage.getItem("feedback-form-state");
+    return storageData ? JSON.parse(storageData) : null;
+  };
+
+  const updateLocalStorage = throttle(() => {
+    const formData = {
+      email: input.value,
+      message: textarea.value,
+    };
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+  }, 500);
+
+  const memoryInfo = getFormDataFromStorage();
+if (memoryInfo !== null) {
+  const { email, message } = memoryInfo;
+  input.value = email;
+  textarea.value = message;
 }
 
-form.addEventListener("submit", function (event){
-    event.preventDefault();
-    if(input.value===" "||textarea.value===" "){
-        alert ("Заповніть усі форми")
-    } else {const storage = LocalStorage.getItem("feedback-form-state");
-console.log(JSON.parse(storage));
-localStorage.removeItem("feedback-form-state")
-input.value=" "
-textarea.value=" "}
-}) 
+input.addEventListener("input", updateLocalStorage);
+textarea.addEventListener("input", updateLocalStorage);
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (input.value.trim() === "" || textarea.value.trim() === "") {
+    alert("Заповніть усі форми");
+  } else {
+    localStorage.removeItem("feedback-form-state");
+    input.value = "";
+    textarea.value = "";
+  }
+});
