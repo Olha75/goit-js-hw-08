ae94285910978050ffbf7bd78224a414d1d0b04a
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-17 21:01:12 +0200

    8.2-change

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,20 +2,17 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
+    const player = new Player(iframe);
+   
+   player.on('timeupdate', throttle((e) =>{
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(e.seconds));
+}, 1000));
 
-    const player = new Vimeo.Player(iframe);
-    const LOCALSTORAGE_KEY = 'videoplayer-current-time';
-
-   player.on('timeupdate', throttle(playOn, 1000));
-
-function playOn(el) {
-    localStorage.setItem(LOCAL_KEY, JSON.stringify(el.seconds));
-}
-
-const time = localStorage.getItem(LOCAL_KEY);
-if (time) {
-  player.setCurrentTime(time);
+const storedTime = localStorage.getItem(LOCALSTORAGE_KEY);
+if (storedTime) {
+  player.setCurrentTime(parseFloat(storedTime));
 }
 
   
