9e02fe0e2366c1554153a9d0b352e0aa9112281c
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-17 21:18:07 +0200

    8.2-e-evnt

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,17 +2,13 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const LOCALSTORAGE_KEY = 'videoplayer-current-time';
-
-    const player = new Player(iframe);
-   
-   player.on('timeupdate', throttle((e) =>{
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(e.seconds));
-}, 1000));
+const LOCALSTORAGE_KEY = 'videoplayer-current-time'; 
+const player = new Player(iframe);
 
 const storedTime = localStorage.getItem(LOCALSTORAGE_KEY);
 if (storedTime) {
   player.setCurrentTime(parseFloat(storedTime));
 }
-
-  
+player.on('timeupdate', throttle((e) => {
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(e.seconds));
+}, 1000));
