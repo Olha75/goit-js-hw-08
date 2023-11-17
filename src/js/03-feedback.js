bf7ba7634eaf854c8e21c5e88ad38e00c959bc53
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-17 20:09:54 +0200

    8.3-add-eventListener

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,3 +3,11 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector(".feedback-form");
 const input = form.querySelector("input[name='email']");
 const textarea = form.querySelector("textarea[name='message']");
+
+const memoryInfo = JSON.parse(LocalStorage.getItem("feedback-form-state"))
+if(memoryInfo !==null){
+    const {email, message}=memoryInfo;
+    input.value = email;
+    textarea.value=message;
+}
+
