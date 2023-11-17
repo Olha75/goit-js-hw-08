90fe1b80ab6e6b0f7ffe298cd500b1f2d55df24c
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-17 20:00:21 +0200

    8.3-perem

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,5 @@
 import throttle from 'lodash.throttle';
+
+const form = document.querySelector(".feedback-form");
+const input = form.querySelector("input[name='email']");
+const textarea = form.querySelector("textarea[name='message']");
